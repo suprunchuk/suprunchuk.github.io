42d1361c957979869456e95d956c9b31f6769928
Pasha Suprunchuk <suprunchuk@internet.ru>
2021-05-15 01:20:12 +0300

    Update mail.php

--- assets/php/mail.php
+++ assets/php/mail.php
@@ -18,7 +18,7 @@
 
         // Set the recipient email address.
         // FIXME: Update this to your desired email address.
-        $recipient = "example@gmail.com ";
+        $recipient = "suprunchuk@internet.ru";
 
         // Build the email content.
         $email_content = "Name: $name\n";
