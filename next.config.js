ecc395dff8b4b965d4e2896e84aefc6e3785446d
Pasha Suprunchuk <suprunchuk@internet.ru>
2023-12-27 00:01:45 +0300

    first commit

---
+++ next.config.js
@@ -0,0 +1,12 @@
+const withNextra = require('nextra')({
+  theme: 'nextra-theme-blog',
+  themeConfig: './theme.config.js',
+  // optional: add `unstable_staticImage: true` to enable Nextra's auto image import
+})
+
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  output: "export",
+};
+
+module.exports = withNextra(nextConfig)
